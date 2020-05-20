b4b45682e70b3e1ab4be98cf950b78cd89e27fbb
Rostykl <Rostyk.lukavyi842@gmaik.com>
2020-05-21 00:24:49 +0300

    api-service

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+connex_app = connexion.App(__name__, specification_dir=basedir)
+main_app = connex_app.app
+
+# Database
+main_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+main_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(main_app)
+# Init ma
+ma = Marshmallow(main_app)
